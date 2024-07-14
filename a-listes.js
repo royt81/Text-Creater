24ffcb0d1a7795f5006cdeb10de7d31ae89c74c8
royt81 <107740928+royt81@users.noreply.github.com>
2024-07-14 09:45:47 +0200

    updates

--- a-listes.js
+++ a-listes.js
@@ -2,21 +2,8 @@
 
 // the secound take 
 
-
-
-const testA = [
-    ['hi!', 'testA1'],
-    ['my name', 'testA2'],
-    ['is Roy', 'testA3']
-]
-const testB = [
-    ['hello!', 'testB1'],
-    ['we are', 'testB2'],
-    ['still here', 'testB3']
-]
-
 const cancellation = [
-    ['cancellation'],
+    ['cancellation', '30vh'],
     ['Kündigung', `Hallo {%customer.firstName,fallback=%},
     
 vielen Dank für deine Anfrage.
@@ -43,20 +30,19 @@ Du erhältst innerhalb von 6 Werktagen eine Widerrufsbestätigung.
     
 Ich wünsche dir einen tollen Tag.`]
 ]
-   
 const beNice = [
-    ['be nice!'],
+    ['be nice!', '30vh'],
     ['call us!',`Gerne kannst du uns bei weiteren Fragen telefonisch kontaktieren.
 Wir sind von Mo - Fr zwischen 9:00 und 17:00 Uhr unter 040 593622030 erreichbar.`],
     ['-4,99',`Als kleines Dankeschön werden wir deine Seriengebühr für den Monat XXX in Höhe von 4,99 € erlassen.`],
     ['feedback',`Wir würden uns sehr freuen, wenn du uns eine Bewertung hinterlassen würdest. 
 Hier ist der Link daz: https://de.trustpilot.com/review/rabot-charge.de.
 Dein Feedback ist uns wichtig!`],
+    ['Code', `Mit der Bewerbungscode CWZBIYS erhaltest du bei Anmeldung eine 50 € Gutschrift von uns.`],
 
 ]
-
 const headlines = [
-    ['Headlines'],
+    ['Headlines', '60vh'],
     ['Unser Gespräch', 'Unser Gespräch'],
     ['Dein Wechsel', 'Dein Wechsel zu Rabot-Charge'],
     ['Deine Anfrage', 'Deine Anfrage'],
@@ -64,9 +50,9 @@ const headlines = [
     ['Dein Wideruf', 'Dein Wideruf V/K'],
     ['Deine Kündigung', 'Deine Kündigung V/K'],
     ['Rechnungsk.', 'Rechnungskorrektur'],
-  ]
+]
 const headlinesEN = [
-    ['subjects'],
+    ['subjects', '60vh'],
     ['Our discotion', 'Our discotion'],
     ['Your transfer', 'Your transfer to Rabot-Charge'],
     ['Your inquery','Your inquery'],
@@ -74,9 +60,8 @@ const headlinesEN = [
     ['Your cancelation', 'Your cancelation'],
     ['Your termanation','Your termanation']
 ]
-
 const template = [
-    ['templates'],
+    ['templates', '30vh'],
     ['nette Gespräch', `Hallo {%customer.firstName,fallback=%},
     
 vielen Dank für das nette Gespräch.
@@ -93,7 +78,7 @@ Bitte entschuldige unsere späte Rückmeldung, wir haben derzeit ein erhöhtes M
 Solltest du weitere Fragen haben, komme gerne auf mich zu. Deine E-Mail landet direkt bei mir und ich würde mich dann sofort bei dir melden.
     
 Ich wünsche dir einen tollen Tag.`],
-    ['Fachabteilung', `Hallo {%customer.firstName,fallback=%},
+    ['FA', `Hallo {%customer.firstName,fallback=%},
     
 ich habe eine Rückantwort aus der Fachabteilung erhalten.
       
@@ -101,29 +86,64 @@ ich habe eine Rückantwort aus der Fachabteilung erhalten.
 Ich wünsche dir einen schönen Tag`
       ]
 ]
+const quickNeed = [
+    ['need', '60vh'],
+    ['JV', `Bitte teile mir deinen neuen, gewünschten Jahresverbrauch mit. Ich werde ihn im System aktualisieren.`],
+    ['pay Rc', `Bitte überweise die Rechnungsnummer XXX auf die folgende IBAN mit dem folgenden Verwendungszweck.`],
+    ['ZS', `Bitte teile mir deinen aktuellen Zählerstand mit.`],
+    ['IBAN', 'Bitte teile mir deine neue IBAN mit.'],
+    ['ZF', `Kannst du uns bitte ein Bild von deinem Zähler schicken?`],
+    ['Kontoausz.', `Bitte teile mir die Kontoauszüge für den XXXX und XXXX Abschläge.`],
+    ['email', `Bitte teile mir deine neue gewünschte E-Mail-Adresse mit.`],
 
+]
+const quickGot = [
+    ['got', '40vh'],
+    ['info MP', `Ich habe diese Information an unseren Marktpartner weitergeleitet. Sobald ich Rückantwort habe, melde ich mich zeitnah bei dir.`],
+    ['ZS', `Ich habe die von dir angegebenen Zählerstände an unseren Marktpartner weitergeleitet.`],
+    
+]
 const quickTelNotes = [
-    ['quick notes'],
+    ['quick notes', '40vh'],
     ['Kollegen besprochen', 'G: Das Thema wurde bereits mit eiem Kollegen besprochen'],
     ['WP Status', 'G: KD rief an, um den Status seiner WP zu erfahren'],
     ['Früher anfangen', `G: KD wollte früher bei uns anfangen.
 V: KD verlangt nach SoKü von AV, Vertrag bei uns widerrufen und ein neuer Vertrag schlissen.`],
 ]
-
 const firstLine = 'Hallo {%customer.firstName,fallback=%},';
 
-// const secoundLineEmail = 'Vielen Dank für deine Anfrage.'; 
-// const secoundLinePhone = 'Vielen Dank für das nette Gespräch.';
-// const secoundLineFA = 'ich habe eine Rückantwort aus der Fachabteilung erhalten.';
-
 const secoundLine = [
-    ['second line'],
+    ['second line', '30vh'],
     ['email', 'Vielen Dank für deine Anfrage.'],
     ['phone', 'Vielen Dank für das nette Gespräch.'],
     ['fachabt.', 'ich habe eine Rückantwort aus der Fachabteilung erhalten.']
 ]
-
-
+const need = [
+    ['need', '60vh'],
+    ['JV'],
+    ['ZS'],
+    ['ÜP'],
+    ['ZF'],
+    ['ZF & ÜP'],
+    ['IBAN'],
+    ['Kontoauszug']
+]
+const want = [
+    ['want', '60vh'],
+    ['JV',['JV me!']],
+    ['ZS'],
+    ['ÜP'],
+    ['ZF'],
+    ['ZF & ÜP'],
+    ['IBAN'],
+    ['Kontoauszug']
+]
+const otherEmailRelated = [
+    ['other', '30vh'],
+    ['late', 'sorry i am late'],
+    ['recommend', 'say nice stuff about me!'],
+    
+]
 
 
 // main list - to be use to build the sectionButton
@@ -137,9 +157,9 @@ const secoundList = [
 ]
 
 const theirdList = [
-    quickTelNotes
+    quickTelNotes, quickGot, quickNeed
 ]
 
 const forthList =[
-    secoundLine
+    secoundLine, need, want, otherEmailRelated
 ]
--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function run(){
 
     container.appendChild(manuHolder)
 
-    const buttonA = buildSectionButton('Headlines', firstList)
-    const buttonB = buildSectionButton('Cancellation', secoundList)
+    const buttonA = buildSectionButton('Headlines', firstList, 'subSection-A')
+    const buttonB = buildSectionButton('Cancellation', secoundList, 'subSection-A')
     const buttonC = buildSectionButton('quick notes', theirdList)
     const buttonD = buildSpcialSectionButton('text-builder')
     const buttonE = buildSectionButton('hallo! 5')
@@ -42,7 +42,7 @@ function buildSectionButton(name, list, align){
 
     button.addEventListener('mouseenter', ()=>{
         button.classList.add('SectionButtonWide')
-        addSubSections(name, list);
+        addSubSections(name, list, align);
     })
     button.addEventListener('mouseleave', ()=>{
         button.classList.remove('SectionButtonWide')
@@ -51,7 +51,7 @@ function buildSectionButton(name, list, align){
     manuHolder.appendChild(button)
 }
 
-function addSubSections(name, list){
+function addSubSections(name, list, align){
 
     const section = document.getElementById(name);
     section.innerHTML = '';
@@ -61,22 +61,26 @@ function addSubSections(name, list){
         const subSubSectionID = `ss${name}${i}`
         subSection.id = subSubSectionID;
 
-        const subSectionTitle = list[i][0];
+        const subSectionTitle = list[i][0][0];
+        const subSectionHeigth = list[i][0][1];
+        //console.log(subSectionHeigth)
         subSection.innerText = subSectionTitle;
 
         subSection.classList.add('subSection');
+        subSection.classList.add(align)
         section.appendChild(subSection);
 
         subSection.addEventListener('mouseenter', ()=>{
             subSection.classList.add('subSectionExpend');
+            subSection.style.height = subSectionHeigth;
             createListItem(list, i, subSubSectionID, subSectionTitle)
         })
         subSection.addEventListener('mouseleave', ()=>{
             subSection.classList.remove('subSectionExpend');
             subSection.innerHTML = '';
             subSection.innerText = subSectionTitle
+            subSection.style.height = 'var(--sub-section-height)'
         })
-    
     }
 }
 function createListItem(list, listID, subSubSectionID, subSectionTitle){
@@ -94,9 +98,8 @@ function createListItem(list, listID, subSubSectionID, subSectionTitle){
 
         listItem.addEventListener('click', ()=>{
             navigator.clipboard.writeText(listItemValue);
+            //subSection.classList.remove('subSectionExpend');
         })
-        //console.log(listItemValue)testB1testA2testB1testA2
-
         subSection.appendChild(listItem)
     }
 }
@@ -115,16 +118,16 @@ function buildSpcialSectionButton(name){
         button.classList.remove('SectionButtonWide');
         button.innerHTML = name;
     })
-    manuHolder.appendChild(button);
+    
+    
 
     const sentence = '';
 
     const lateButton = document.createElement('div');
 
-
+    manuHolder.appendChild(button);
 }
 function addSpcialSubSections(name, list){
-    
     const section = document.getElementById(name);
     section.innerHTML = '';
 
@@ -133,22 +136,55 @@ function addSpcialSubSections(name, list){
         const subSubSectionID = `ss${name}${i}`
         subSection.id = subSubSectionID;
 
-        const subSectionTitle = list[i][0];
+        const subSectionTitle = list[i][0][0];
+        const subSectionHeigth = list[i][0][1];
         subSection.innerText = subSectionTitle;
-
-        subSection.classList.add('subSection');
+        subSection.classList.add('specialSubSection');
         section.appendChild(subSection);
 
-        subSection.addEventListener('mouseenter', ()=>{
-            subSection.classList.add('subSectionExpend');
-            createListItem(list, i, subSubSectionID, subSectionTitle)
+        subSection.addEventListener('mouseenter', async ()=>{    
+            try{
+                subSection.classList.add('subSectionExpend');
+                subSection.classList.add('subSection-B');
+                subSection.style.height = subSectionHeigth;
+                await createSpcialListItem(list, i, subSubSectionID, subSectionTitle);
+            } catch (error){
+                console.error('things go bad in spcial-sub-section!');
+            }
         })
         subSection.addEventListener('mouseleave', ()=>{
             subSection.classList.remove('subSectionExpend');
+            subSection.classList.remove('subSection-B');
             subSection.innerHTML = '';
-            subSection.innerText = subSectionTitle
+            subSection.innerText = subSectionTitle;
+            subSection.style.height = 'var(--sub-section-height)'
         })
-    
     }
+    const runButton = document.createElement('div');
+    runButton.classList.add('specialSubSection');
+    runButton.id = 'runButton';
+    runButton.innerText = 'RUN'
 
+    section.appendChild(runButton)
 }
+
+function createSpcialListItem(list, listID, subSubSectionID, subSectionTitle){
+
+    const subSection = document.getElementById(subSubSectionID)
+    subSection.innerHTML = subSectionTitle; 
+
+    for( let i=1; i<list[listID].length; i++){
+        const listItem = document.createElement('div');
+        listItem.classList.add('SpcialListItem');
+        listItem.innerText = list[listID][i][0];
+        const listItemValue = list[listID][i][1];
+
+        listItem.addEventListener('click', ()=>{
+            navigator.clipboard.writeText(listItemValue);
+        })
+        subSection.appendChild(listItem)
+    }
+}
+
+
+
